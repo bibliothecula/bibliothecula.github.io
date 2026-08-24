c27655558ddaf2998a0a31e53efdcc87b87cd716
the-81-creeds <adityanaiya8125@gmail.com>
2026-08-24 22:38:00 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,7 +106,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Bibliothetica, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Bibliothecula, Inc.`,
       },
       prism: {
         theme: prismThemes.github,
