e7e87ffd485f66fe9819eeabf3ec9c8f58852837
the-81-creeds <adityanaiya8125@gmail.com>
2026-08-25 10:16:18 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Bibliothecula',
-  tagline: 'An Open Source Library on Microbiology',
+  tagline: 'Trying to Index Resources for Biology at All Levels Big and Small, So You Don\'t Spend Hours Searching',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
